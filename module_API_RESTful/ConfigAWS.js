ba96cc335fc21410ad9e346ff70854e43e07668b
Ân Đinh - Sandmark <hongan12@gmail.com>
2017-12-26 22:59:17 +0700

    Revert "update IAM"
    This reverts commit beea8c18d4298faf9a3794f7a6940f977e9b595a.

--- module_API_RESTful/ConfigAWS.js
+++ module_API_RESTful/ConfigAWS.js
@@ -1,4 +1,8 @@
 var AWS = require("aws-sdk");
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of beea8c1... update IAM
 exports.configAWS = function () {
 
 //work with DynamoDB local
@@ -12,14 +16,8 @@ exports.configAWS = function () {
 //work with DynamoDB Online
     AWS.config.update({
         region: "us-east-2",
-        endpoint: "dynamodb.us-east-2.amazonaws.com",
-        accessKeyId : "AKIAIROGLNBW44Y4CFHA",
-        secretAccessKey : "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"
+        endpoint: "dynamodb.us-east-2.amazonaws.com"
     });
-//     AWS.config.update({
-//         region: "us-east-2",
-//         endpoint: "dynamodb.us-east-2.amazonaws.com"
-//     });
-//     AWS.config.accessKeyId = "AKIAJBCPGOJHUSX3EIIA";
-//     AWS.config.secretAccessKey = "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";
+    AWS.config.accessKeyId = "AKIAJBCPGOJHUSX3EIIA";
+    AWS.config.secretAccessKey = "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";
 }
