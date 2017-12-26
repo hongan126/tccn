84ffbb9037c633c7fa540f2b6c3c6208fc1ab5cd
Ân Đinh - Sandmark <hongan12@gmail.com>
2017-12-26 23:01:26 +0700

    update IAM 4

--- module_API_RESTful/ConfigAWS.js
+++ module_API_RESTful/ConfigAWS.js
@@ -1,8 +1,5 @@
 var AWS = require("aws-sdk");
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of beea8c1... update IAM
 exports.configAWS = function () {
 
 //work with DynamoDB local
@@ -18,6 +15,6 @@ exports.configAWS = function () {
         region: "us-east-2",
         endpoint: "dynamodb.us-east-2.amazonaws.com"
     });
-    AWS.config.accessKeyId = "AKIAJBCPGOJHUSX3EIIA";
-    AWS.config.secretAccessKey = "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";
+    AWS.config.accessKeyId = "AKIAIROGLNBW44Y4CFHA";
+    AWS.config.secretAccessKey = "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";
 }
